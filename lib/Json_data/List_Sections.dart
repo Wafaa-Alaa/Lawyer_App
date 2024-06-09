5b3e82df44d6ac3f1d2bd6db4c59a2545b3f3760
Abdallah-Abdalmonem <bedo30176@gmail.com>
2024-06-09 15:50:54 +0300

    add chat for section

--- lib/Json_data/List_Sections.dart
+++ lib/Json_data/List_Sections.dart
@@ -1,72 +1,9 @@
-import'package:flutter/material.dart';
-
-List section=[
-  {
-    'title':Text('تجاري',style: TextStyle(
-      fontWeight: FontWeight.w400,
-      color: Colors.white,
-      fontSize: 25,
-    ),),
-    'color':Color(0xff66280E),
-  },
-  {
-    'title':Text('جنائي',style: TextStyle(
-      fontWeight: FontWeight.w400,
-      color: Colors.white,
-      fontSize: 25,
-  ),),
-    'color':Color(0xff8B5D49),
-  },
-  {
-  'title':Text('مدني',style: TextStyle(
-    fontWeight: FontWeight.w400,
-    color: Colors.white,
-    fontSize: 25,
-  ),),
-  'color':Color(0xff66280E),
-  },
-  {
-  'title':Text('تعويض',style: TextStyle(
-    fontWeight: FontWeight.w400,
-    color: Colors.white,
-    fontSize: 25,
-  ),),
-  'color': Color(0xff8B5D49),
-  },
-  {
-  'title':Text('عام',style: TextStyle(
-    fontWeight: FontWeight.w400,
-    color: Colors.white,
-    fontSize: 25,
-  ),),
-  'color':Color(0xff66280E),
-  },
-  {
-  'title':Padding(
-    padding: EdgeInsets.all(12),
-    child: Text('قضايا ادارية ومجلس الدولة',style: TextStyle(
-      fontWeight: FontWeight.w400,
-      color: Colors.white,
-      fontSize: 25,
-    ),),
-  ),
-
-  'color':Color(0xff8B5D49),
-  },
-  {
-  'title':Text('عقاري',style: TextStyle(
-    fontWeight: FontWeight.w400,
-    color: Colors.white,
-    fontSize: 25,
-  ),),
-  'color':Color(0xff66280E),
-  },
-
-
-
-
-
-
-
-
+List<String> section = [
+  'تجاري',
+  'جنائي',
+  'مدني',
+  'تعويض',
+  'عام',
+  'قضايا ادارية ومجلس الدولة',
+  'عقاري',
 ];
--- lib/Pages/Section_Page.dart
+++ lib/Pages/Section_Page.dart
@@ -11,6 +11,23 @@ class SectionPage extends StatefulWidget {
 }
 
 class _SectionPageState extends State<SectionPage> {
+  late TextEditingController searchController;
+  @override
+  void initState() {
+    searchController = TextEditingController();
+    searchList = section;
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  bool isearch = false;
+  List searchList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,57 +39,103 @@ class _SectionPageState extends State<SectionPage> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Sections',style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 25
-                    ),),
-                    IconButton(onPressed:(){},
-                        icon:Icon(Icons.search,size: 35,)
+                    Text(
+                      'Sections',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
                     ),
                   ],
+                ),
+                TextFormField(
+                  controller: searchController,
+                  onChanged: (value) {
+                    setState(() {
+                      isearch = true;
+                    });
+                    // search for section
+                    searchList = section
+                        .where((element) => element.contains(value))
+                        .toList();
+                    print(searchList);
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    hintStyle: TextStyle(fontSize: 20),
+                    suffixIcon: isearch
+                        ? InkWell(
+                            onTap: () {
+                              searchController.clear();
+                              setState(() {
+                                isearch = false;
+                                searchList = section;
+                              });
+                            },
+                            child: Icon(
+                              Icons.clear,
+                              size: 35,
+                            ),
+                          )
+                        : Icon(
+                            Icons.search,
+                            size: 35,
+                          ),
+                  ),
                 ),
                 SizedBox(
                   height: 20,
                 ),
                 GestureDetector(
-                  onTap: (){
-                   Navigator.push(context, MaterialPageRoute(builder:(_)=>DashboardPage()));
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => DashboardPage()));
                   },
-                  child: Wrap(
-                   children: List.generate(section.length, (index){
-                     return GestureDetector(
-                       onTap: (){
-                         Navigator.pushNamed(context,'SpecilizationPage');
-                       },
-                       child: Padding(
-                         padding: const EdgeInsets.only(right: 8,bottom: 8),
-                         child: Container(
-                           height: 200,
-                           width: 100,
-                           decoration: BoxDecoration(
-                             borderRadius: BorderRadius.circular(30),
-                             color: section[index]['color'],
-
-                           ),
-                           child:Center(
-                             child: section[index]['title'],
-                           )
-
-                         ),
-                       ),
-                     );
-                   })
-                  ),
+                  child: searchList.isEmpty
+                      ? Text('No Sections Found',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900))
+                      : Wrap(
+                          children: List.generate(
+                            searchList.length,
+                            (index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                      context, 'SpecilizationPage');
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 8, bottom: 8),
+                                  child: Container(
+                                      height: 200,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: index % 2 == 0
+                                            ? Color(0xff66280E)
+                                            : Color(0xff8B5D49),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          searchList[index],
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.white,
+                                            fontSize: 25,
+                                          ),
+                                        ),
+                                      )),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                 )
               ],
             ),
           ),
         ),
       ),
-
-
-      );
+    );
   }
 }
